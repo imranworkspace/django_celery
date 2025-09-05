d0a205a95a5487d51dd5b86bfa6667ac5b85fd75
imranlatur24 <imranlatur24@gmail.com>
2025-09-05 12:33:54 +0530

    added task name for specific tasks

--- Celery_ORM_Cache/myapp/tasks.py
+++ Celery_ORM_Cache/myapp/tasks.py
@@ -1,12 +1,12 @@
 from celery import shared_task
 from time import sleep
 
-@shared_task
+@shared_task(name="subtraction_task")
 def sub(x,y):
     sleep(10)
     return x-y
 
-@shared_task
+@shared_task(name="addition_task")
 def add(x,y):
     sleep(20) # holding for 20 seconds
     return x+y
--- Celery_ORM_Cache/myapp2/tasks.py
+++ Celery_ORM_Cache/myapp2/tasks.py
@@ -1,7 +1,7 @@
 from celery import shared_task
 from time import sleep
 
-@shared_task
+@shared_task(name="multiplication_task")
 def mul(x,y):
     sleep(10) # holding for 20 seconds
     return x*y
